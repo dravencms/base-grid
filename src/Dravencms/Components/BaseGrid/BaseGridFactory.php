57d0511444d0a8e2c0b5d90b7dd9ea4e8f8db0c7
Adam Schubert <adam.schubert@sg1-game.net>
2021-10-14 18:29:00 +0200

    chore: Fix

--- src/Dravencms/Components/BaseGrid/BaseGridFactory.php
+++ src/Dravencms/Components/BaseGrid/BaseGridFactory.php
@@ -5,14 +5,11 @@
 
 namespace Dravencms\Components\BaseGrid;
 
-use Nette\ComponentModel\IContainer;
 
 interface BaseGridFactory
 {
     /**
-     * @param IContainer $container
-     * @param $name
      * @return BaseGrid
      */
-    public function create(IContainer $container = null, $name = null);
+    public function create();
 }

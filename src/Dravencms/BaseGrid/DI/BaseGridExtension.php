9c5a40feea594873cd1d93f6721e697e6f7e5c3d
Adam Schubert <adam.schubert@sg1-game.net>
2021-10-14 17:38:33 +0200

    fix: Add BaseGridFactory as service

--- src/Dravencms/BaseGrid/DI/BaseGridExtension.php
+++ src/Dravencms/BaseGrid/DI/BaseGridExtension.php
@@ -13,17 +13,30 @@ class BaseGridExtension extends CompilerExtension
     public function loadConfiguration(): void
     {
         $this->loadComponents();
-
+        $this->loadModels();
     }
 
     protected function loadComponents(): void
     {
         $builder = $this->getContainerBuilder();
         foreach ($this->loadFromFile(__DIR__ . '/components.neon') as $i => $command) {
-            $cli = $builder->addFactoryDefinition($this->prefix('components.' . $i))
-                ->setAutowired(false);
+            $cli = $builder->addFactoryDefinition($this->prefix('components.' . $i));
             if (is_string($command)) {
                 $cli->setImplement($command);
+            } else {
+                throw new \InvalidArgumentException;
+            }
+        }
+    }
+
+    protected function loadModels(): void
+    {
+        $builder = $this->getContainerBuilder();
+        foreach ($this->loadFromFile(__DIR__ . '/models.neon') as $i => $command) {
+            $cli = $builder->addDefinition($this->prefix('models.' . $i))
+                ->setAutowired(false);
+            if (is_string($command)) {
+                $cli->setFactory($command);
             } else {
                 throw new \InvalidArgumentException;
             }

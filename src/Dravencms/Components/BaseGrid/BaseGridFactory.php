0886afa186cb4f6b456e865d86b24a2205750e65
Adam Schubert <adam.schubert@sg1-game.net>
2021-10-15 05:50:32 +0200

    fix: Rename BaseGrid to BaseGridFactory

--- src/Dravencms/Components/BaseGrid/BaseGridFactory.php
+++ src/Dravencms/Components/BaseGrid/BaseGridFactory.php
@@ -1,15 +1,26 @@
 <?php declare(strict_types = 1);
-/**
- * Copyright (C) 2016 Adam Schubert <adam.schubert@sg1-game.net>.
- */
 
 namespace Dravencms\Components\BaseGrid;
 
+use Dravencms\Components\BaseControl\BaseControl;
+use Nette\ComponentModel\IContainer;
 
-interface BaseGridFactory
+/**
+ * Copyright (C) 2016 Adam Schubert <adam.schubert@sg1-game.net>.
+ */
+class BaseGridFactory extends BaseControl
 {
+
     /**
-     * @return BaseGrid
+     * @param IContainer $container
+     * @param $name
+     * @return Grid
      */
-    public function create();
+    public function create(IContainer $container = null, $name = null): Grid
+    {
+        $grid = new Grid($container, $name);
+        $grid->setRememberState(true);
+        return $grid;
+    }
+
 }
